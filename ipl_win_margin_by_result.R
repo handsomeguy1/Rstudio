54a2bb457060674766f3871a474cb88076775110
handsomeguy1 <109041396+handsomeguy1@users.noreply.github.com>
2022-07-23 02:30:11 +0530

    Update ipl_win_margin_by_result.R

--- ipl_win_margin_by_result.R
+++ ipl_win_margin_by_result.R
@@ -5,11 +5,16 @@ library(lubridate)
 
 i %>% 
   select(-method, - eliminator, - umpire1,- umpire2,- neutral_venue, 
-         winner, toss_decision, toss_winner, result_margin, result, venue) %>% 
-  mutate(win_margin = ifelse(result_margin>50, "big", "small")) %>% 
+         winner, toss_decision, toss_winner, result_margin, result, venue) %>%
+  drop_na(result) %>% 
+  mutate(win_margin = ifelse(result_margin>50, "big", "small")) %>%
+  drop_na(win_margin) %>% 
 relocate(win_margin, .after = winner) %>% 
   ggplot(aes(win_margin))+
-  geom_bar(aes(fill = result))
+  geom_bar(aes(fill = result), width = 0.5)+
+  scale_y_continuous(limits = c(0,900))+
+  theme_bw()
+  
 
 
 
