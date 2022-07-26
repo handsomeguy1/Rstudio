ed4d333bfea5c01ea520d4fe370c60155b04ed3e
handsomeguy1 <109041396+handsomeguy1@users.noreply.github.com>
2022-07-27 02:23:36 +0530

    Create A'bad Districts, Bihar & Mah.R

---
+++ A'bad Districts, Bihar & Mah.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(ggtext)
+rm(c1, c2)
+c <- as.tibble(c)
+
+c %>% 
+  select(State.Name, District.Name, Area.Name, Material_Floor_Mud, Rural.Urban) %>% 
+  filter(State.Name %in% c("MAHARASHTRA", "BIHAR") & District.Name == "Aurangabad" & Rural.Urban !="Total") %>% 
+  relocate(Rural.Urban, .before = Material_Floor_Mud) %>% 
+ ggplot(aes(State.Name, Material_Floor_Mud, fill = (Rural.Urban)))+
+  geom_bar(stat = "identity", width = 0.5)+
+  labs(x="", y="Houses with Mud Flooring", title = "Aurangabad Districts: Bihar & Maharashtra")+
+  scale_fill_discrete(name = "Rural.Urban")+
+  geom_richtext(aes(label = Material_Floor_Mud, fontface = "bold", vjust = 1))+
+  theme_classic()
+
