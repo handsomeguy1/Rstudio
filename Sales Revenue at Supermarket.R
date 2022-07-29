2a4bd1cc633594b855943ea08e4cc4a38b6fa2c0
handsomeguy1 <109041396+handsomeguy1@users.noreply.github.com>
2022-07-29 17:55:08 +0530

    Create Sales Revenue at Supermarket.R

---
+++ Sales Revenue at Supermarket.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(lubridate)
+library(ggtext)
+
+s<- supermarket_sales_Sheet1
+
+str(d$Date)         
+
+d %>% 
+  mutate(year = year(Date),
+         month = month(Date),
+         day = day(Date)) %>%  
+  select(month, `gross income`, `Product line`, Gender, Payment) %>% 
+  group_by(`Product line`)  %>% 
+  arrange(desc(month))  %>% 
+  summarise(total_revenue = sum(`gross income`)) %>%  
+  ggplot(aes( `Product line`,total_revenue))+
+  geom_bar(stat = "identity", width = 0.5, fill = "darkblue")+
+  geom_richtext(aes(label = total_revenue, vjust = -0.3, fontface = "bold"))+
+  labs(title = "Revenue from the Sales (in $)", x = "", y= "Revenue")+
+  coord_flip()
+  theme_bw()+
+  theme(
+       axis.text = element_text(face = "bold")
+  )
+
+
+
+
+
+
