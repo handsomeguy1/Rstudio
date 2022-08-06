e0c381756384ea9cc408992e61770e81c6faa80b
handsomeguy1 <109041396+handsomeguy1@users.noreply.github.com>
2022-08-06 12:34:38 +0530

    Str_detect revised

--- Substr letter start with end with.R
+++ Substr letter start with end with.R
@@ -6,5 +6,10 @@ m %>%
   select(genus, order) %>% 
   filter(str_detect(genus,"^Ca")) %>% 
   filter(str_detect(order, "la")) %>% view()
-         
+ 
+# Tried a more simplified code for the same
+
+m %>% 
+  select(genus, order) %>%
+  filter(str_detect(genus,"^Ca") & str_detect (order,"ia$"))
           
