4796bed9c869186610923c88c7ff641abd3e00f2
handsomeguy1 <109041396+handsomeguy1@users.noreply.github.com>
2022-08-07 02:28:51 +0530

    Update Replacing NA by col mean.R

--- Replacing NA by col mean.R
+++ Replacing NA by col mean.R
@@ -16,6 +16,25 @@ mean_age <- mean(t$Age, na.rm = T)
 #Replacing NA with column mean in mean_age
 t[is.na(t$Age), "Age"] <- mean_age
 
+# Replacing NA in msleep dataset
+library(tidyverse)
+
+msleep
+
+#First calculate mean:
+
+mean_sleep_cycle <- mean(m$sleep_cycle, na.rm = T)
+mean_brn_wt <- mean(m$brainwt, na.rm = T)
+mean_slp_rm <- mean(m$sleep_rem, na.rm = T)
+
+# Then replace NA:
+
+m[is.na(m$sleep_cycle), "sleep_cycle"] <- mean(m$sleep_cycle, na.rm = T)
+m[is.na(m$brainwt), "brainwt"] <- mean(m$brainwt, na.rm = T)
+m[is.na(m$sleep_rem), "sleep_rem"] <- mean(m$sleep_rem, na.rm = T)
+view(m)
+
+
 
 
 
