9638d152d18ce45b1a5207d1f93c9c6859e3ff6f
handsomeguy1 <109041396+handsomeguy1@users.noreply.github.com>
2022-08-07 01:38:51 +0530

    Update Replacing NA by col mean.R

--- Replacing NA by col mean.R
+++ Replacing NA by col mean.R
@@ -9,9 +9,11 @@ t$Ticket<- as.character(t$Ticket)
 t$Parch<- as.factor(t$Parch)
 t$Sex<- as.factor(t$Sex) 
 t$Pclass<- as.factor(t$Pclass)
+
+#Calculating mean age in the Age column
 mean_age <- mean(t$Age, na.rm = T)
 
-#Replacing NA with column mean
+#Replacing NA with column mean in mean_age
 t[is.na(t$Age), "Age"] <- mean_age
 
 
